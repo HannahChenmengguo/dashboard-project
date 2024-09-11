c83ba76e9f67f0ddb19747f9ac55e6aa233b0b97
HannahChenmengguo <168465176+HannahChenmengguo@users.noreply.github.com>
2024-09-10 22:16:41 -0700

    header

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// Initialize modules
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask() {
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))  // Catch and log errors
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask() {
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync Serve
+function browserSyncServe(done) {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  done();  // Signal that the task is complete
+}
+
+// Browsersync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();  // Signal that the task is complete
+}
+
+// Watch Task
+function watchTask() {
+  watch('*.html', browserSyncReload);
+  watch(
+    ['app/scss/**/*.scss', 'app/**/*.js'],
+    series(scssTask, jsTask, browserSyncReload)
+  );
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask, jsTask);
